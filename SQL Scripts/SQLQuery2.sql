329fe13933a3c0da0b348bb9f0c572d0f075f5ce
Valkata583 <v.dupinov@gmail.com>
2023-07-21 16:47:39 +0300

    Unity file

---
+++ SQL Scripts/SQLQuery2.sql
@@ -0,0 +1,8 @@
+USE [Project_database]
+GO
+
+SET IDENTITY_INSERT [dbo].[Game] ON 
+    INSERT INTO [dbo].[Game] (Id) VALUES(2)
+GO
+
+
--- VoluyakAPI/Controllers/InfoController.cs
+++ VoluyakAPI/Controllers/InfoController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VoluyakAPI.Models;
 
 namespace VoluyakAPI.Controllers
 {
@@ -7,11 +9,50 @@ namespace VoluyakAPI.Controllers
     [ApiController]
     public class InfoController : ControllerBase
     {
+        private readonly ProjectDatabaseContext _context;
+        private readonly ProjectDatabaseContext _points;
+        public InfoController(ProjectDatabaseContext context)
+        {
+            _context = context;
+           // _points = points;
+        }
+
+        public InfoController(ProjectDatabaseContext points)
+        {
+            _points = points;
+        }
+
         [HttpPost]
-        public IActionResult GetGameDetails([FromBody] string id)
+        public IActionResult GetGameDetails([FromBody] int id)
         {
 
+            var data = _context.Games.Find(id);
+
+
+
+
+
+
+            //var desc = new Description() { Title = "Test", Descripe = "Dneska hodiujaisdjkasdhkjasjhkdaksjh kjhasd kjhaskjhd askjhd kjh" };
+
+
+            //desc.Title = "Test 2";
+            //var data2 = _context.Descriptions.Add(desc);
+
+
+
+
+
+            //_context.SaveChanges();
             return Ok(id.ToString());
         }
+
+        public IActionResult CountPoints([FromBody] int id)
+        {
+            var p =_points.Leaderbords.Find(id);
+            _points.Points += -_points;
+            return Ok(id.ToString());
+        }
+
     }
 }
